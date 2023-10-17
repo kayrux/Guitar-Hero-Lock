024abda6f375f082a16c1af0a132a1c5e46d7efc
Surya Kusjanto <skusjanto@outlook.com>
2023-10-16 22:38:25 -0600

    Added the fixed buttons and detect hits.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,19 +5,76 @@ const fallAreaElement = document.getElementById("fall-area");
 const fallLane1Element = document.getElementById("fall-lane-1");
 const fallLane2Element = document.getElementById("fall-lane-2");
 const fallLane3Element = document.getElementById("fall-lane-3");
+let fallingButtons = [];
 
-const startButtonElement = document.getElementById("start-button");
+const trigger1Element = document.getElementById("trigger-1");
+const trigger2Element = document.getElementById("trigger-2");
+const trigger3Element = document.getElementById("trigger-3");
+const triggers = [trigger1Element, trigger2Element, trigger3Element];
+
+// const startButtonElement = document.getElementById("start-button");
 let fallingButtonsClicked = [];
 const passcode = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+const startTriggers = [0, 0, 0];
 
 let counter = 0;
 const maxExecutions = 4;
 let intervalId = 0;
+let awaitingStart = true;
+setTriggerBackgroundState(0);
+
+/* ******************************************************************************************* */
+function setTriggersToGameMode() {
+  for (let i = 0; i < triggers.length; i++) {
+    triggers[i].classList.add("game-mode-" + (i + 1));
+  }
+}
+
+function setTriggersToWaitMode() {
+  for (let trigger of triggers) {
+    trigger.classList.add("wait-mode");
+  }
+}
+
+function resetTriggersBackground() {
+  for (let trigger of triggers) {
+    trigger.className = "";
+  }
+}
+
+function addDefaultClassesToTrigger() {
+  for (let trigger of triggers) {
+    trigger.classList.add("guitar-button");
+  }
+}
+
+function setTriggersText(text) {
+  for (let trigger of triggers) {
+    trigger.innerHTML = text;
+  }
+}
+
+function setTriggerBackgroundState(state) {
+  resetTriggersBackground();
+  addDefaultClassesToTrigger();
+  switch (state) {
+    case 0:
+      setTriggersText("Tap to start");
+      setTriggersToWaitMode();
+      break;
+    case 1:
+      setTriggersText("");
+      setTriggersToGameMode();
+  }
+}
+/* ******************************************************************************************* */
 
 function start() {
-  startButtonElement.remove();
+  // startButtonElement.remove();
   addFallingButtons();
   intervalId = setInterval(addFallingButtons, 3000);
+  awaitingStart = false;
+  setTriggerBackgroundState(1);
 }
 
 function clearFallLanes() {
@@ -28,6 +85,7 @@ function clearFallLanes() {
 
 function addFallingButtons() {
   clearFallLanes();
+  fallingButtons = [];
 
   counter++;
   console.log(`This is execution number ${counter}`);
@@ -42,10 +100,11 @@ function addFallingButtons() {
     const newButton = document.createElement("div");
     newButton.classList.add("falling-button");
     newButton.id = buttonId;
-    newButton.addEventListener("click", function () {
-      onFallingButtonClick(buttonId);
-    });
+    // newButton.addEventListener("click", function () {
+    //   onFallingButtonClick(buttonId);
+    // });
     parentElement.appendChild(newButton);
+    fallingButtons.push(newButton);
     fallingButtonsClicked.push(0);
   }
 }
@@ -94,16 +153,102 @@ function passcodeSequenceFinished() {
 }
 
 function getFallButtonIndexFromId(id) {
-  return id.substring(id.length - 1, id.length);
+  return id.substring(id.indexOf("-", id.length - 3) + 1, id.length);
 }
 
-function onFallingButtonClick(id) {
-  console.log("button " + id + " clicked");
-  fallingButtonsClicked[getFallButtonIndexFromId(id)] = 1;
-}
+// function onFallingButtonClick(id) {
+//   console.log("button " + id + " clicked");
+//   fallingButtonsClicked[getFallButtonIndexFromId(id)] = 1;
+// }
 
 function reset() {
   fallingButtonsClicked = [];
+  for (let i = 0; i < startTriggers.length; i++) {
+    startTriggers[i] = 0;
+  }
   counter = 0;
-  guitarBackgroundElement.appendChild(startButtonElement);
+  awaitingStart = true;
+  setTriggerBackgroundState(0);
+  // guitarBackgroundElement.appendChild(startButtonElement);
+}
+
+/* ******************************************************************************************************************************** */
+
+function animateButton(element, buttonNumber) {
+  switch (buttonNumber) {
+    case 1:
+      element.classList.add("flash-red");
+      break;
+    case 2:
+      element.classList.add("flash-blue");
+      break;
+    case 3:
+      element.classList.add("flash-green");
+      break;
+  }
+
+  setTimeout(() => {
+    element.classList.remove("flash-red", "flash-blue", "flash-green");
+  }, 500);
+}
+
+// My idea is display the score on the button itself, but not finish yet.
+// function checkHit(buttonNumber) {
+//   let note = document.querySelector(".note:nth-child(" + buttonNumber + ")");
+//   let buttonTop = document
+//     .querySelectorAll(".guitar-button")
+//     [buttonNumber - 1].getBoundingClientRect().top;
+//   let noteTop = note.getBoundingClientRect().top;
+
+//   if (Math.abs(buttonTop - noteTop) < 30) {
+//     score += 10;
+//     document.querySelectorAll(".guitar-button")[buttonNumber - 1].innerText =
+//       "Button " + buttonNumber + " (" + score + ")";
+//   }
+// }
+
+function startConditionsMet() {
+  for (let i of startTriggers) {
+    if (i == 0) return false;
+  }
+  return true;
+}
+
+function checkHit(buttonNumber, element) {
+  let marginOfError = 50;
+  let fallingButton = fallingButtons[buttonNumber - 1];
+  let hitBox = fallingButton.getBoundingClientRect();
+  let targetRect = element.getBoundingClientRect();
+
+  if (
+    targetRect.top > hitBox.top - marginOfError &&
+    targetRect.bottom < hitBox.bottom + marginOfError
+  ) {
+    console.log("HIT", fallingButton);
+    fallingButtonsClicked[getFallButtonIndexFromId(fallingButton.id)] = 1;
+  }
+}
+
+function checkHitAndAnimate(buttonNumber, element) {
+  // checkHit(buttonNumber);
+  animateButton(element, buttonNumber);
+  if (awaitingStart) {
+    startTriggers[buttonNumber - 1] = 1;
+    if (buttonNumber == 1) {
+      element.classList.add("ready-1");
+    } else if (buttonNumber == 2) {
+      element.classList.add("ready-2");
+    } else {
+      element.classList.add("ready-3");
+    }
+    element.innerHTML = "Ready";
+
+    if (startConditionsMet()) {
+      console.log("Start conditions met");
+      start();
+    }
+    console.log("Start conditions not met");
+  } else {
+    checkHit(buttonNumber, element);
+  }
 }
